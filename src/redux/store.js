ccfb8f640361b607bf702b6dda24c8444a23b7c0
Vladislav Shevchuk <109815421+vladikshevchuk@users.noreply.github.com>
2023-04-20 15:17:37 +0200

    implement redux persist, work with localStorage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsSlice } from './contactsSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: 'contacts',
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, contactsSlice.reducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsSlice.reducer
+    contacts: persistedContactsReducer,
   },
 });
+
+export const persistor = persistStore(store);
